63b00bfe562934f3f1b1360a62a54b8a32bc82a9
Lattice <159420767+Lattice23@users.noreply.github.com>
2025-07-08 12:23:22 -0400

    Update keybinds.lua

--- lua/kak/keybinds.lua
+++ lua/kak/keybinds.lua
@@ -8,9 +8,12 @@ function M.setup(opts)
   local word = { "w", "e", "b" }
   utils.keymap.set(word, { presets = { "reselect" } })
   
-  -- Use "none" preset for basic movements (acts like normal Vim)
+  -- All basic movements exit visual mode (no extending selections)
   local movement = { "h", "j", "k", "l" }
-  utils.keymap.set(movement, { presets = { "none" } })
+  utils.keymap.set(movement, { presets = { "deselect" } })
+  
+  -- Implement v key with deselect behavior
+  utils.keymap.set("v", { presets = { "deselect" } })
   
   -- Keep the normal mode operators
   for _, key in ipairs({ "d", "c", "y" }) do
