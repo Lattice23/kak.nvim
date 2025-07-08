658d00728dba010be68637dfdcc35974dacea95f
Lattice <159420767+Lattice23@users.noreply.github.com>
2025-07-08 12:16:44 -0400

    Update keybinds.lua

--- lua/kak/keybinds.lua
+++ lua/kak/keybinds.lua
@@ -1,19 +1,21 @@
 local M = {}
-
 local utils = require("kak.utils")
 
 function M.setup(opts)
   local opts = opts or {}
-
-  -- Only set up word motions with reselect behavior in NORMAL mode
+  
+  -- Word motions with reselect behavior
   local word = { "w", "e", "b" }
-  for _, key in ipairs(word) do
-    vim.keymap.set("n", key, "v" .. key .. "<Esc>", { noremap = true })
-  end
-
-  -- Normal mode operators: select, then act
+  utils.keymap.set(word, { presets = { "reselect" } })
+  
+  -- Use "none" preset for basic movements (acts like normal Vim)
+  local movement = { "h", "j", "k", "l" }
+  utils.keymap.set(movement, { presets = { "none" } })
+  
+  -- Keep the normal mode operators
   for _, key in ipairs({ "d", "c", "y" }) do
-    vim.keymap.set("n", key, "v" .. key, { noremap = true })
+    vim.keymap.set({ "n" }, key, "v" .. key)
   end
 end
+
 return M
